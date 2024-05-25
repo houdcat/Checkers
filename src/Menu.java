63860a9dabb6b045b0d95c9a27ef9c586ecafa15
fh <houdek2@spsejecna.cz>
2024-05-25 20:22:05 +0200

    Changing program icon and adding the first buttons for the menu

--- src/Menu.java
+++ src/Menu.java
@@ -2,13 +2,26 @@ import javax.swing.*;
 
 public class Menu {
 
-    JFrame menuFrame = new JFrame();
-
+    JFrame menuFrame = new JFrame("Checkers");
+    JButton rulesButton = new JButton("Rules");
 
     Menu(){
         this.menuFrame.setSize(750, 750);
         this.menuFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        this.menuFrame.setLayout(null);
+        this.menuFrame.setLocationRelativeTo(null);
+        ImageIcon icon = new ImageIcon("icon.png");
+        this.menuFrame.setIconImage(icon.getImage());
+
+
+        this.rulesButton.setFocusable(false);
+        this.rulesButton.setBorder(BorderFactory.createEtchedBorder());
+        this.rulesButton.setBounds(62,500, 200, 100);
+        this.menuFrame.add(rulesButton);
+
         this.menuFrame.setVisible(true);
+
 
 
     }
